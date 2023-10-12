e7e87cf4469d7605a378e3f162bbfd6ec3bc7960
Jericho Rechter <104209298+JerichoRechter@users.noreply.github.com>
2023-10-12 19:16:32 +0300

    Questions.jsx

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -27,6 +27,106 @@ export default function QuestionList({loadQuestions }){
                 text:"Mulan",
                 correct:false
               }]},
+          {
+            id:2,
+            question:"Which question is this?",
+            answers:[
+              {
+                text:"1",
+                correct:false
+              },
+              {
+                text:"2",
+                correct:true
+              },
+              {
+                text:"3",
+                correct:false
+              },
+              {
+                text:"4",
+                correct:false
+              }]},
+          {
+            id:3,
+            question:"Which question is this?",
+            answers:[
+              {
+                text:"4",
+                correct:false
+              },
+              {
+                text:"3",
+                correct:true
+              },
+              {
+                text:"8",
+                correct:false
+              },
+              {
+                text:"6",
+                correct:false
+              }]},
+          {
+            id:4,
+            question:"Which question is this?",
+            answers:[
+              {
+                text:"6",
+                correct:false
+              },
+              {
+                text:"4",
+                correct:true
+              },
+              {
+                text:"7",
+                correct:false
+              },
+              {
+                text:"9",
+                correct:false
+              }]},
+          {
+            id:5,
+            question:"Which question is this?",
+            answers:[
+              {
+                text:"3",
+                correct:false
+              },
+              {
+                text:"5",
+                correct:true
+              },
+              {
+                text:"4",
+                correct:false
+              },
+              {
+                text:"7",
+                correct:false
+              }]},
+          {
+            id:6,
+            question:"Balodan ayrılırken camdan bir ayakkabı bırakan Disney karakteri kimdir?",
+            answers:[
+              {
+                text:"Pocahontas",
+                correct:false
+              },
+              {
+                text:"Cindirella1",
+                correct:true
+              },
+              {
+                text:"Pamuk Prenses",
+                correct:false
+              },
+              {
+                text:"Mulan",
+                correct:false
+              }]},
               {
                 id:13,
                 question:"Pick One",
